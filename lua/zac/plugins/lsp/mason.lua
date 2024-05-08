9b2e853eece0cb64fb065b216111f2011552f19b
Zacarie Carr <zacarie.carr@acidgreen.com.au>
2024-05-08 17:04:05 +1000

    Updated plugins

--- lua/zac/plugins/lsp/mason.lua
+++ lua/zac/plugins/lsp/mason.lua
@@ -1,46 +1,46 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✔",
-          package_pending = "➜",
-          package_uninstalled = "✘",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✔",
+					package_pending = "➜",
+					package_uninstalled = "✘",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "eslint",
-        "denols",
-        "tsserver",
-        "lua_ls",
-        "tailwindcss",
-        "html",
-        "astro",
-        "rust_analyzer",
-        "graphql",
-        "cssls",
-        "jsonls",
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"eslint",
+				"denols",
+				"tsserver",
+				"lua_ls",
+				"tailwindcss",
+				"html",
+				"astro",
+				"rust_analyzer",
+				"graphql",
+				"cssls",
+				"jsonls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "stylua",
-        "eslint_d",
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"eslint",
+			},
+		})
+	end,
 }
