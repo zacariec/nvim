2a0adb98ee64ff4c40d6274c5490723fe6305063
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2025-04-08 10:45:52 +1000

    Added conform for formatting

---
+++ lua/zac/plugins/conform.lua
@@ -0,0 +1,42 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "mason.nvim" },
+	lazy = true,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>cF",
+			function()
+				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				liquid = { "prettier" },
+				astro = { "prettier" },
+				yaml = { "prettier" },
+				json = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
