c57094b4d10c891587834042bd5e7ed6bb205337
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2025-04-08 10:44:58 +1000

    Added folding to nvim instance

---
+++ lua/zac/plugins/nvim-ufo.lua
@@ -0,0 +1,23 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = {
+		"kevinhwang91/promise-async",
+	},
+	config = function()
+		local ufo = require("ufo")
+
+		vim.o.foldcolumn = "1"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		vim.keymap.set("n", "zO", ufo.openAllFolds)
+		vim.keymap.set("n", "zC", ufo.closeAllFolds)
+
+		ufo.setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "treesitter", "indent" }
+			end,
+		})
+	end,
+}
