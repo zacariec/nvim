48ffdc3b68d129cb1f2fb969f301a0e3255e9657
zacariec <10770942+zacariec@users.noreply.github.com>
2025-07-18 14:59:10 +1000

    Added set options

--- lua/zac/set.lua
+++ lua/zac/set.lua
@@ -2,17 +2,35 @@ local set = vim.opt
 local bo = vim.bo
 local wo = vim.wo
 local lsp = vim.lsp
+local cmd = vim.cmd
 
+-- Sets
 set.tabstop = 2
 set.shiftwidth = 2
+set.autochdir = true
+set.autoindent = true
+set.endofline = true
+set.wrap = false
+set.showmatch = true
+set.number = true
+set.relativenumber = true
+set.termguicolors = true
+set.incsearch = true
+set.hlsearch = true
+set.smartcase = true
+set.backspace = 'indent,start,eol'
+
+
+-- Buffer Options
 bo.expandtab = true
+
+-- Window Options
 wo.number = true
 wo.cursorline = true
 
 vim.diagnostic.config({
 	update_in_insert = true,
 	vim.diagnostic.config({ virtual_lines = { prefix = "🔥" } })
-
 })
 
 lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
@@ -26,3 +44,13 @@ lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
 		update_in_insert = true,
 	}
 )
+
+-- auto source vim
+cmd([[
+  augroup neovim
+		autocmd!
+		autocmd BufWritePost $MYVIMRC nested source $MYVIMRC
+	augroup end
+]])
+
+
