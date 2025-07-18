c91cfde5a89b1c045305c52909a0d20bcd71af82
zacariec <10770942+zacariec@users.noreply.github.com>
2025-07-18 14:59:43 +1000

    Fixed lazygit int

--- lua/zac/plugins/lazygit.lua
+++ lua/zac/plugins/lazygit.lua
@@ -1,17 +1,17 @@
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<CR>", desc = { "Open lazy git" } },
-  },
+	"kdheepak/lazygit.nvim",
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<leader>lg", "<cmd>LazyGit<CR>", desc = "Open lazy git" },
+	},
 }
