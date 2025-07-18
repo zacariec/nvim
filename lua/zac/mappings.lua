fc3b4dc77de700f3c4b0c58c9c012322b0859961
zacariec <10770942+zacariec@users.noreply.github.com>
2025-07-18 14:59:28 +1000

    Added some preferable keybinds

---
+++ lua/zac/mappings.lua
@@ -0,0 +1,20 @@
+local action = require("lspsaga.codeaction");
+
+-- Open Code action
+vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true, noremap = true })
+vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga code_action<CR>", { silent = true, noremap = true })
+-- Jump to next diagnostic error.
+vim.keymap.set("n", "<leader>]", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true, noremap = true })
+-- Jump to previous diagnostic error
+vim.keymap.set("n", "<leader>[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true, noremap = true })
+-- Open float terminal from LSP Saga
+vim.keymap.set("n", "<leader>te", "<cmd>Lspsaga open_floaterm<CR>", { silent = true, noremap = true })
+-- Close foat terminal from LSP Saga
+vim.keymap.set("t", "<leader>te", "<C-\\><C-n><cmd>Lspsaga close_floaterm<CR>", { silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { silent = true, noremap = true })
+
+for i = 10, 1, -1
+  do
+    vim.keymap.set("n", "<C-1>", "<cmd>buffer"..i.."<CR>", { silent = true, noremap = true })
+  end
